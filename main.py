fc21b8690fe935a076d5c23d89702fd217d44373
Yasin Ünal <yasin.unal@bil.omu.edu.tr>
2022-09-26 01:52:02 +0300

    fazlalıklar atıldı

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-from ast import Pass
 from my_info import * 
 from selenium import webdriver 
 from selenium.webdriver.common.keys import Keys 
 from selenium.webdriver.common.by import By
 
-import pyautogui
 import time
 
 class UBS_Savar():
